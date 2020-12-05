038aaab15c8b237e42c65a4a0e02bff1ed02a097
Lexirem <me.vallribera@gmail.com>
2020-12-05 21:16:10 +0100

    primera ruta profile

--- routes/profile.js
+++ routes/profile.js
@@ -2,3 +2,14 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user');
+
+
+router.get('/:id', async(req, res, next) => {
+   let myUser = await User.findById(req.params.id);
+   try{
+    res.json(myUser);
+   } catch (err){
+    console.log(err);
+   }
+});
+
