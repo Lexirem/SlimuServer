652ce05f8214e26b3ea5493be73dfe0c39c00934
Lexirem <me.vallribera@gmail.com>
2020-12-15 09:01:17 +0100

    new deploy

---
+++ public/precache-manifest.98aad344c936dd95060e20a3f8f4156c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47bb4f290a81fcbcaa46048fafc08431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "079d8ca7c72f71515765",
+    "url": "/static/css/main.d94e48b5.chunk.css"
+  },
+  {
+    "revision": "f7391db032c349233f8d",
+    "url": "/static/js/2.1a9a92b7.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1a9a92b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079d8ca7c72f71515765",
+    "url": "/static/js/main.562db227.chunk.js"
+  },
+  {
+    "revision": "5179ff928fc7dafdbfe9",
+    "url": "/static/js/runtime-main.81fc7dff.js"
+  }
+]);
